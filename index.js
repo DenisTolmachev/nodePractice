af168f430c9e240c2a385fdc414f0ac0190b7773
Denis <denis.tolmachev@gmail.com>
2022-09-19 20:50:30 +0300

    add exemple

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 const express = require('express');
+const app = express();
+
+const PORT = 8080;
+
+app.use((req, res, next) => {
+    console.log(`${req.method} ${req.originalUrl}, ${new Date().toISOString()}`);
+    next();
+});
+
+app.get('/home', (req, res) => {
+    res.json({javascript: 'object'});
+});
+
+app.listen(PORT, (err) => {
+    if (err) {
+        console.error('Error starting server', err);
+    }
+    console.log(`Server started on port ${PORT}`);
+});
